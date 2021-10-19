e04b40cb47d9ee30b7d460d680b6b2920b938b14
SkyLiNer823 <flamesky823@gmail.com>
2021-10-19 23:14:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -42,80 +42,82 @@ def callback():
         return "OK"
 
 
+def text_reply(content, event):
+    reply = TextSendMessage(text=content)
+    line_bot_api.reply_message(event.reply_token, reply)
+
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    get_message = event.message.text
-
-    def text_reply(content):
-        reply = TextSendMessage(text=content)
-        line_bot_api.reply_message(event.reply_token, reply)
-
-    if isinstance(event, MessageEvent):
-        interface = FlexSendMessage(
-            alt_text='test',
-            contents={
-                "type": "bubble",
-                "hero": {
-                    "type": "image",
-                    "url": "https://cdn.unwire.hk/wp-content/uploads/2020/10/1028-1b.jpg",
-                    "size": "full",
-                    "aspectRatio": "20:13",
-                    "aspectMode": "cover",
-                    "action": {
-                        "type": "uri",
-                        "uri": "http://linecorp.com/"
-                    }
-                },
-                "body": {
-                    "type": "box",
-                    "layout": "vertical",
-                    "contents": [
-                        {
-                            "type": "text",
-                            "text": "功能選單",
-                            "weight": "bold",
-                            "size": "xl"
-                        }
-                    ]
-                },
-                "footer": {
-                    "type": "box",
-                    "layout": "vertical",
-                    "spacing": "sm",
-                    "contents": [
-                        {
-                            "type": "button",
-                            "action": {
-                                "type": "postback",
-                                "label": "下單/上架",
-                                "data": "A&func1"
-                            },
-                            "style": "primary"
-                        },
-                        {
-                            "type": "button",
-                            "style": "secondary",
-                            "action": {
-                                "type": "postback",
-                                "label": "線上比價",
-                                "data": "A&func2"
-                            }
-                        },
-                        {
-                            "type": "button",
-                            "action": {
-                                "type": "postback",
-                                "label": "物流追蹤",
-                                "data": "A&func3"
-                            },
-                            "style": "secondary"
-                        }
-                    ],
-                    "flex": 0
+    #get_message = event.message.text
+    interface = FlexSendMessage(
+        alt_text='test',
+        contents={
+            "type": "bubble",
+            "hero": {
+                "type": "image",
+                "url": "https://cdn.unwire.hk/wp-content/uploads/2020/10/1028-1b.jpg",
+                "size": "full",
+                "aspectRatio": "20:13",
+                "aspectMode": "cover",
+                "action": {
+                    "type": "uri",
+                    "uri": "http://linecorp.com/"
                 }
+            },
+            "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                    {
+                        "type": "text",
+                        "text": "功能選單",
+                        "weight": "bold",
+                        "size": "xl"
+                    }
+                ]
+            },
+            "footer": {
+                "type": "box",
+                "layout": "vertical",
+                "spacing": "sm",
+                "contents": [
+                    {
+                        "type": "button",
+                        "action": {
+                            "type": "postback",
+                            "label": "下單/上架",
+                            "data": "A&func1"
+                        },
+                        "style": "primary"
+                    },
+                    {
+                        "type": "button",
+                        "style": "secondary",
+                        "action": {
+                            "type": "postback",
+                            "label": "線上比價",
+                            "data": "A&func2"
+                        }
+                    },
+                    {
+                        "type": "button",
+                        "action": {
+                            "type": "postback",
+                            "label": "物流追蹤",
+                            "data": "A&func3"
+                        },
+                        "style": "secondary"
+                    }
+                ],
+                "flex": 0
             }
-        )
-        line_bot_api.reply_message(event.reply_token, interface)
-    elif isinstance(event, PostbackEvent):
-        if event.postback.data == "A&func1":  # 如果回傳值為「購買商品」
-            text_reply('func1')
+        }
+    )
+    line_bot_api.reply_message(event.reply_token, interface)
+
+
+@handler.add(PostbackEvent)
+def handle_postback(event):
+    data = event.postback.data
+    text_reply(data, event)
